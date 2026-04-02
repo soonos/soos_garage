1d37c997eb4dd8b34db14609f8f1a30f13a2e4a6
Leo <lol.pommes1@web.de>
2026-04-02 18:33:07 +0200

    Add garage system config, manifest, and SQL
    Initial add of a job garage system: adds config.lua with global settings (insurance, impound, blips, webhook) and a list of predefined garages (spawn/delete/impound points, jobs, types). Adds fxmanifest.lua declaring resource metadata, scripts, and dependencies (es_extended, oxmysql) and enabling lua54. Adds soos_garage.sql creating an owned_vehicles table (owner, plate PK, vehicle, job, model, stored, type, insured) for persisting vehicles.

---
+++ fxmanifest.lua
@@ -0,0 +1,28 @@
+fx_version 'cerulean'
+games {'gta5'}
+lua54 'yes'
+
+author 'Soos'
+description 'Job Garage System'
+version '1.0.0'
+
+client_scripts {
+  'client/*.lua'
+}
+
+server_scripts {
+  'server/*.lua',
+  '@oxmysql/lib/MySQL.lua'
+}
+
+shared_scripts {
+  '@es_extended/imports.lua',
+  '@es_extended/locale.lua',
+  'config.lua',
+  'locales/*.lua'
+}
+
+dependencies {
+  'es_extended',
+  'oxmysql'
+}
